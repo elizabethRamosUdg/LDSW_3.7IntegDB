de78079793e571981aa349198795bf49436334e3
Elizabeth Ramos <elizabeth.ramos@udgvirtual.udg.mx>
2025-05-11 14:23:18 -0600

    Include firestore configurations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     //id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services")
 }
 
 dependencies {
@@ -26,7 +27,7 @@ dependencies {
 android {
     namespace = "com.example.ldsw_elizabeth_ramos"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
